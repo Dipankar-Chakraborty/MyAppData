016b4760b9d90bd803bc319f72f896f3ee871f6f
Dipankar Chakraborty <dipankarchakraborty645@gmail.com>
2019-09-28 15:13:09 +0530

    Added getter and setter in Book.java

--- Book.java
+++ Book.java
@@ -1,4 +1,16 @@
 class Book{
 	private int bookId;
 	private String name;
+	public setbookId(int bookId){
+		this.bookId=bookId;
+	}
+	public getbookId(){
+		return bookId;
+	}
+	public setname(String name){
+		this.name=name;
+	}
+	public getname(){
+		return name;
+	}
 }
